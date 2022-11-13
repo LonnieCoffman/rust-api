fdcd62854cea44646952cfaa050a875dabf51cb1
Lonnie Coffman <lonniecoffman@icloud.com>
2022-11-13 07:00:57 -0600

    basic liveness endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/liveness", web::get().to(health_check)))
+        .bind(("127.0.0.1", 6005))?
+        .run()
+        .await
 }
